8a6d124763cb6a4067d8b8177462469a44301c66
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-01-15 22:29:00 -0600

    ++

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>');
+});
+
+app.listen(3000);
+
+/*
+const  server = http.createServer(app);
+
+server.listen(3000);
+*/
