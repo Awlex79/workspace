2b4bbfa3f46df3d70f53d833f4f3105a2271774a
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-01-25 08:09:16 -0600

    Handlebars finished

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,13 @@ const products = [];
 //});
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('add-product', { 
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product',
+        activeAddProduct: true, 
+        formsCSS: true,
+        productCSS: true
+     });
 
 });
 
