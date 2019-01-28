350e6055ada4f1fc4557230aff4342550f7cda8d
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-01-27 23:54:03 -0600

    ++

--- app.js
+++ app.js
@@ -2,27 +2,8 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const expressHbs = require('express-handlebars');
-
 const app = express();
 
-//app.engine('pug', require('pug').__express)
-
-/*
-HANDLEBARS
-app.engine(
-    'handlebars',
-    expressHbs({
-        layoutsDir:'views/layouts/', 
-        defaultLayout: 'main-layout',
-        extname: 'handlebars'
-    }));
-
-
-app.set('view engine', 'handlebars');
-*/
-
-//app.set('view engine', 'pug');
 
 app.set('view engine', 'ejs' );
 app.set('views', 'views');
@@ -39,7 +20,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', {pageTitle: '404'});
 });
 
