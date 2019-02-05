559ae6ef7a668d4f9670579e56263a474efe6aee
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-02-04 22:56:42 -0600

    ++

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+//const products = [];
+const fs = require('fs');
+const path = require('path');
+//const root = require('../util/path');
 
 module.exports = class Product {
     constructor(t) {
@@ -6,7 +9,22 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        //products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename), 
+            'data',
+            'products.json'
+        );
+
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.write(p, JSON.stringify(products), (err) => {
+                console.log(err);    
+            });
+        });
     }
 
     static fetchAll() {
--- util/path.js
+++ util/path.js
@@ -1,2 +1,2 @@
 const path = require('path');
-module.exports = path.dirname(process.mainModule.filename)
+exports.pathDir = path.dirname(process.mainModule.filename)
