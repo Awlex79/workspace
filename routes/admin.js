ef21965b7fa389a0d9b952f3e19b8be305970572
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-02-08 22:55:49 -0600

    ++

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require ('express');
 const router = express.Router();
-const productsController = require('../controllers/products.js');
+const adminController = require('../controllers/admin.js');
 
 // admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// admin/products => GET
+router.get('/products')
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct); 
+router.post('/add-product', adminController.postAddProduct); 
 
 module.exports = router;
 
