75460baa043a2a1b113715ab1b075d9ca77dd289
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-02-15 22:39:30 -0600

    ++

---
+++ models/cart.js
@@ -0,0 +1,18 @@
+
+
+module.exports = class Cart {
+    /* 
+    constractor() {
+        this.products = [];
+        this.totalPrice = 0;
+    }
+    */
+
+    static addProduct() {
+     // ferch the previous cart
+
+
+     //analyse the cart => find existing product
+     // add new product/increase quantity   
+    }
+}
