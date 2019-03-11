ced5c7b6414fbc235b5b86a3f9454b0c5dbbd7ec
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2019-03-10 23:46:19 -0500

    database connection

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = reqiire('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node.js-awl',
+    password: '0000'
+});
+
+module.exports = pool.promise();
